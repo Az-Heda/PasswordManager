ed7cd48f409c6c5f285c055a1d44594ba8531f1f
Az-Heda <tecno19dragon@gmail.com>
2024-11-16 11:52:53 +0100

    Updated frontend

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 const { invoke } = window.__TAURI__.core;
 
-console.log('Hi?');
-
 async function GetServiceList() {
   const services = await invoke('get_service_list');
   const serviceList = services.replaceAll('\r', '').split('\n').filter(i => i.length > 0);
@@ -9,39 +7,65 @@ async function GetServiceList() {
   return serviceList;
 }
 
-async function GetPassword(password, service) {
-  const hash = await invoke('passwordify', { password, service })
-    .catch(err => greetMsgEl.innerHTML = err);
+async function AddService(service) {
+  return invoke('add_service', { service });
+}
+
+async function GetPassword(password, service, maxLength) {
+  const hash = await invoke('passwordify', { password, service, maxLength })
+    .catch(err => alert(err));
   console.log({ password, service, hash });
   return hash;
 }
 
+
 window.addEventListener("DOMContentLoaded", async () => {
   const servicesGrid = document.querySelector('#services');
   const passwordInput = document.querySelector('#password');
+  const addService = document.querySelector('#new-service');
   console.log(passwordInput)
   const passwords = {};
 
-  servicesGrid.innerHTML = '';
-  for (const service of await GetServiceList()) {
-    const label = document.createElement('span');
-    const pws = document.createElement('span');
-    const btn = document.createElement('button');
+  const FetchPasswords = async () => {
+    for (const [service, tag] of Object.entries(passwords)) {
+      tag.innerHTML = (prev == '') ? '' : await GetPassword(prev, service, 16);
+    }
+  }
+  const ShowAllServices = async () => {
+    Object.keys(passwords).map(e => delete passwords[e]);
+    servicesGrid.innerHTML = '';
+    for (const service of await GetServiceList()) {
+      const label = document.createElement('span');
+      const pws = document.createElement('span');
+      const btn = document.createElement('button');
 
-    label.innerHTML = service;
-    btn.innerHTML = 'Copy';
-    pws.classList.add('truncate', 'max-w-32');
-    btn.classList.add('bg-zinc-399', 'hover:bg-green-500', 'dark:bg-zinc-700', 'dark:hover:bg-green-700', 'border', 'border-zinc-700', 'dark:border-zinc-300', 'transition-colors', 'duration-300');
+      label.innerHTML = service;
+      btn.innerHTML = 'Copy';
+      pws.classList.add('truncate', 'max-w-32');
+      btn.classList.add('bg-zinc-400', 'hover:bg-green-500', 'dark:bg-zinc-700', 'dark:hover:bg-green-700', 'border', 'border-zinc-700', 'dark:border-zinc-300', 'transition-colors', 'duration-300');
 
-    passwords[service] = pws;
+      passwords[service] = pws;
 
-    servicesGrid.appendChild(label);
-    servicesGrid.appendChild(pws);
-    servicesGrid.appendChild(btn);
+      servicesGrid.appendChild(label);
+      servicesGrid.appendChild(pws);
+      servicesGrid.appendChild(btn);
 
-    btn.onclick = () => {
-      navigator.clipboard.writeText(pws.innerText);
-    };
+      btn.onclick = () => {
+        navigator.clipboard.writeText(pws.innerText);
+      };
+    }
+  }
+
+  addService.onclick = async () => {
+    const minLength = 4;
+    const newService = prompt(`Insert service name: \n(min length: ${minLength})`);
+    if (newService.length < minLength) {
+      alert(`ERROR\nLength: ${newService.length}\nMin length: ${minLength}`)
+      return;
+    }
+    await AddService(newService)
+    await ShowAllServices();
+    await FetchPasswords();
   }
 
   let prev = '';
@@ -52,10 +76,9 @@ window.addEventListener("DOMContentLoaded", async () => {
       return;
     }
     if (passwordInput.value == prev) return;
-
     prev = passwordInput.value;
-    for (const [service, tag] of Object.entries(passwords)) {
-      tag.innerHTML = await GetPassword(prev, service);
-    }
+    await FetchPasswords();
   };
+
+  ShowAllServices();
 });
