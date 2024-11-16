13403f6c3b5c00e1f95d8351a8245fb1812b3175
Az-Heda <tecno19dragon@gmail.com>
2024-11-16 11:52:58 +0100

    Updated backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,7 +10,7 @@ use base64::{self, Engine};
 const SERVICE_FILENAME: &str = "services.txt";
 
 #[tauri::command]
-fn passwordify(password: &str, service: &str) -> String {
+fn passwordify(password: &str, service: &str, max_length: usize) -> String {
     let mut b64 =
         base64::prelude::BASE64_STANDARD.encode(String::from(service) + service + service);
     b64 = b64.replace("=", "");
@@ -18,11 +18,17 @@ fn passwordify(password: &str, service: &str) -> String {
     let argon2 = Argon2::default();
     let hash = argon2.hash_password(password.as_bytes(), s).unwrap();
     let hash_string = hash.to_string();
-    let mut parts = hash_string.split("$").collect::<Vec<_>>();
-    for _ in 0..4 {
-        parts.remove(0);
+    let parts = hash_string.split("$").collect::<Vec<_>>();
+    let psw = String::from(parts.last().copied().unwrap());
+    if psw.len() < max_length {
+        return psw;
     }
-    parts.join("$").to_string()
+    let mut partial_password = String::new();
+    let chars: Vec<_> = psw.chars().collect();
+    while partial_password.len() < max_length {
+        partial_password.push(chars[partial_password.len()]);
+    }
+    partial_password
 }
 
 #[tauri::command]
@@ -31,12 +37,17 @@ fn get_service_list() -> String {
     content
 }
 
+#[tauri::command]
+fn add_service(service: &str) -> String {
+    save_service(service)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![passwordify, get_service_list])
+        .invoke_handler(tauri::generate_handler![passwordify, get_service_list, add_service])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -56,12 +67,12 @@ fn get_services() -> String {
     content
 }
 
-#[allow(dead_code)]
-fn save_service(service: &str) {
+fn save_service(service: &str) -> String {
     check_service_path();
     let mut content: String = get_services();
     if !content.contains(service) {
         content += format!("{}\n", service).as_str();
-        fs::write(SERVICE_FILENAME, content).unwrap();
+        fs::write(SERVICE_FILENAME, content.clone()).unwrap();
     }
+    content
 }
